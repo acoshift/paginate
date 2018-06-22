916306897dab86c43375ffb8931825e45db42246
acoshift <acoshift@gmail.com>
2018-06-22 23:37:05 +0700

    add movable paginate

--- paginate_test.go
+++ paginate_test.go
@@ -116,3 +116,63 @@ func TestPages(t *testing.T) {
 	assert.Equal(t, []int64{1, 2, 0, 6}, paginate.New(1, 1, 6).Pages(1, 1))
 	assert.Equal(t, []int64{1, 2, 0, 5, 6}, paginate.New(1, 1, 6).Pages(1, 2))
 }
+
+func TestNewMovable(t *testing.T) {
+	p := paginate.NewMovable(0, -1, 100)
+	assert.Equal(t, int64(1), p.Page())
+	assert.Equal(t, int64(1), p.PerPage())
+	assert.Equal(t, int64(0), p.Offset())
+	assert.Equal(t, int64(101), p.Limit())
+	assert.Equal(t, int64(100), p.Count())
+	assert.Equal(t, int64(100), p.MaxPage())
+	assert.False(t, p.CanPrev())
+	assert.True(t, p.CanNext())
+	{
+		limit, offset := p.LimitOffset()
+		assert.Equal(t, p.Limit(), limit)
+		assert.Equal(t, p.Offset(), offset)
+	}
+	p.SetCount(10)
+	assert.Equal(t, int64(10), p.Count())
+
+	p = paginate.NewMovable(1, 10, 100)
+	assert.Equal(t, int64(1), p.Page())
+	assert.Equal(t, int64(10), p.PerPage())
+	assert.Equal(t, int64(0), p.Offset())
+	assert.Equal(t, int64(1001), p.Limit())
+	assert.Equal(t, int64(1000), p.Count())
+	assert.Equal(t, int64(100), p.MaxPage())
+	assert.False(t, p.CanPrev())
+	assert.True(t, p.CanNext())
+
+	p = paginate.NewMovable(1, 10, -1)
+	assert.Equal(t, int64(1), p.Page())
+	assert.Equal(t, int64(10), p.PerPage())
+	assert.Equal(t, int64(10), p.Count())
+	assert.Equal(t, int64(1), p.MaxPage())
+
+	assert.Equal(t, int64(10), paginate.NewMovable(1, 5, 10).MaxPage())
+	assert.Equal(t, int64(11), paginate.NewMovable(1, 5, 10).SetCount(52).MaxPage())
+	assert.Equal(t, int64(100), paginate.NewMovable(100, 5, 16).Page())
+	assert.Equal(t, int64(1), paginate.NewMovable(1, 5, 10).Prev())
+	assert.Equal(t, int64(1), paginate.NewMovable(2, 5, 10).Prev())
+	assert.Equal(t, int64(2), paginate.NewMovable(1, 5, 10).Next())
+	assert.Equal(t, int64(4), paginate.NewMovable(4, 5, 0).SetCount(4).Next())
+}
+
+func TestMovablePages(t *testing.T) {
+	assert.Equal(t, []int64{1, 2, 3, 4, 5, 6, 7, 8}, paginate.NewMovable(1, 1, 8).Pages())
+	assert.Equal(t, []int64{6, 7, 8, 9, 10, 11, 12, 13}, paginate.NewMovable(10, 1, 8).Pages())
+	assert.Equal(t, []int64{1, 2, 3, 4, 5}, paginate.NewMovable(1, 1, 5).Pages())
+	assert.Equal(t, []int64{1, 2, 3, 4, 5}, paginate.NewMovable(2, 1, 5).Pages())
+	assert.Equal(t, []int64{1, 2, 3, 4, 5}, paginate.NewMovable(3, 1, 5).Pages())
+	assert.Equal(t, []int64{2, 3, 4, 5, 6}, paginate.NewMovable(4, 1, 5).Pages())
+	assert.Equal(t, []int64{3, 4, 5, 6, 7}, paginate.NewMovable(5, 1, 5).Pages())
+	assert.Equal(t, []int64{4, 5, 6, 7, 8}, paginate.NewMovable(6, 1, 5).Pages())
+	assert.Equal(t, []int64{1, 2, 3, 4}, paginate.NewMovable(1, 1, 4).Pages())
+	assert.Equal(t, []int64{1, 2, 3, 4}, paginate.NewMovable(2, 1, 4).Pages())
+	assert.Equal(t, []int64{1, 2, 3, 4}, paginate.NewMovable(3, 1, 4).Pages())
+	assert.Equal(t, []int64{2, 3, 4, 5}, paginate.NewMovable(4, 1, 4).Pages())
+	assert.Equal(t, []int64{3, 4, 5, 6}, paginate.NewMovable(5, 1, 4).Pages())
+	assert.Equal(t, []int64{4, 5, 6, 7}, paginate.NewMovable(6, 1, 4).Pages())
+}
